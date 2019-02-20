470a86f051e29948aeb3428e554574062bd21233
hugoalcalde <hab1julio>
2019-02-20 18:51:21 +0100

    code for practice 3 finished (working with server, sequences and client)

--- P3 Folfer/client_3.py
+++ P3 Folfer/client_3.py
@@ -4,22 +4,19 @@ import socket
 
 
 # SERVER IP, PORT
-IP = "212.128.253.79"
-PORT = 8081
+IP = "212.128.253.85"
+PORT = 8080
+
+msg = """ACCTTT\nlen\ncomplement\ncountA\npercA\ncountC"""
 
 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-while True:
+# establish the connection to the Server (IP, PORT)
+s.connect((IP, PORT))
 
-
-    msg = input("> ")
-
-
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # establish the connection to the Server (IP, PORT)
-    s.connect((IP, PORT))
-
-    # Send the request message to the server
-    s.send(str.encode(msg))
-    s.close()
+# Send the request message to the server
+s.send(str.encode(msg))
+response = s.recv(2048).decode()
+print(response)
+s.close()
--- P3 Folfer/server_3.py
+++ P3 Folfer/server_3.py
@@ -2,8 +2,8 @@
 from seq_4 import Seq
 import socket
 
-PORT = 8081
-IP = "212.128.253.79"
+PORT = 8080
+IP = "212.128.253.85"
 MAX_OPEN_REQUEST = 5
 
 
@@ -22,11 +22,76 @@ while True:
     #  -- process the client request
     print("Attending client: {}".format(address))
     msg = clientsocket.recv(2048).decode("utf-8")
-    list_msg = msg.split("\\n")
     list_response = []
-    for letter in list_msg[0]:
-        if letter != "A" and letter != "C" and letter != "G" and letter != "T" :
-            list_response.append("NOT VALID")
+    list_msg = msg.split("\n")
+    if list_msg[0] == "" :
+        clientsocket.send(str.encode("ALIVE"))
+        clientsocket.close()
+    else :
+        for letter in list_msg[0]:
+            if letter != "A" and letter != "C" and letter != "G" and letter != "T" :
+                list_response.append("NOT VALID")
+        if len(list_response) == 0 :
+            list_response.append("OK")
+            sequence_client = Seq(list_msg[0])
+            list_msg.remove(list_msg[0])
+            for element in list_msg :
+                print(element)
+                if element == "len" :
+                    response = sequence_client.len()
+                    response = str(response)
+                    list_response.append(response)
+                elif element == "complement" :
+                    response = sequence_client.complement()
+                    response = response.strbases
+                    list_response.append(response)
+                elif element == "reverse" :
+                    response = sequence_client.reverse()
+                    response = response.strbases
+                    list_response.append(response)
+                elif element == "countA" :
+                    response = sequence_client.count("A")
+                    response = str(response)
+                    list_response.append(response)
+                elif element == "countC":
+                    response = sequence_client.count("C")
+                    response = str(response)
+                    list_response.append(response)
+                elif element == "countT":
+                    response = sequence_client.count("T")
+                    response = str(response)
+                    list_response.append(response)
+                elif element == "countG":
+                    response = sequence_client.count("G")
+                    response = str(response)
+                    list_response.append(response)
+                elif element == "percA":
+                    response = sequence_client.perc("A")
+                    response = str(response)
+                    list_response.append(response)
+                elif element == "percC":
+                    response = sequence_client.perc("C")
+                    response = str(response)
+                    list_response.append(response)
+                elif element == "percT":
+                    response = sequence_client.perc("T")
+                    response = str(response)
+                    list_response.append(response)
+                elif element == "percG":
+                    response = sequence_client.perc("G")
+                    response = str(response)
+                    list_response.append(response)
+                else :
+                    response = "ERROR"
+                    list_response.append(response)
+
+        str_send = ""
+        for element in list_response :
+            str_send = str_send + element + "\n"
+        clientsocket.send(str.encode(str_send))
+        clientsocket.close()
+
+
 
 
 
